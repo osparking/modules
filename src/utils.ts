d4417e709c65a6c33042324e350f9c305e831931
BumTiger <jbpark03@gmail.com>
2025-09-23 06:29:25 +0900

    pick a random element from given array

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
 function addNumber(x: number, y: number) : number {
   return x + y;
 }
+
+function pickRandElt<T>(elements: T[]): T {
+  const index = Math.floor(Math.random()* elements.length);
+  return elements[index];
+}
