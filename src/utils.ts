325fcd916e505a062f8c46692be5a2429e73b84f
BumTiger <jbpark03@gmail.com>
2025-09-23 20:38:30 +0900

    returns randomly picked element from array

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
 function add(x: number, y: number): number {
   return x + y;
 }
+
+function pickRandElt<T>(elements: T[]): T {
+  const index = Math.floor(Math.random()* elements.length);
+  return elements[index];
+}
