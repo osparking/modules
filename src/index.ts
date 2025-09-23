c1e43aa5f800900682fdbad05b53854434d1eda1
BumTiger <jbpark03@gmail.com>
2025-09-23 10:43:08 +0900

    const declare error - duplicte decl'

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 console.log(pickRandElt([1, 3, 78, 1000]));
+const x = 1;
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,5 @@ function pickRandElt<T>(elements: T[]): T {
   const index = Math.floor(Math.random()* elements.length);
   return elements[index];
 }
+
+const x = 2;
