3499d60d388a366a3330b3ca9b25e26dc245ac64
BumTiger <jbpark03@gmail.com>
2025-09-23 13:48:24 +0900

    untrack dist folder

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log(pickRandElt([1, 3, 78, 1000]));
--- dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function addNumber(x, y) {
-    return x + y;
-}
-function pickRandElt(elements) {
-    const index = Math.floor(Math.random() * elements.length);
-    return elements[index];
-}
